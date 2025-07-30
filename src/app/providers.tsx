07c113d438afa8790b9831180017b0c0ee77cbe9
jookebawx <sammy@LAPTOP-EOVBL6RM.localdomain>
2025-07-30 20:57:23 +0900

    more fixes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,15 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import { type ReactNode } from "react"
 import config from "@/rainbowKitConfig"
 import { WagmiProvider } from "wagmi"
 import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit"
-import { useState } from "react"
+import {useState} from "react"
 import "@rainbow-me/rainbowkit/styles.css"
 
 export function Providers(props: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
-    if (typeof window !== "undefined") {
-        const client = createWagmiClient(); // or whatever wallet logic you use
-        // continue safely
-    }
-
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
