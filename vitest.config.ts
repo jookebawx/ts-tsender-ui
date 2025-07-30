da7bc638743c2a05309eadb64b1eb8aa9bfc9c3a
jookebawx <110410554+jookebawx@users.noreply.github.com>
2025-07-30 22:39:39 +0900

    fix tests and docs

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'node',
+    exclude: ['test/playwright/**'],
+    include: ['src/**/*.test.ts', 'test/**/*.test.ts'],
   },
 });
