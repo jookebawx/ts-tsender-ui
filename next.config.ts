02ccca791db312d845123ed4ddabb5126f22351d
jookebawx <sammy@LAPTOP-EOVBL6RM.localdomain>
2025-07-30 23:34:55 +0900

    updat next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/ts-tsender-ui" : "",
-  assetPrefix: isProd ? "/ts-tsender-ui/" : "./",
-  trailingSlash: true,
+  basePath: "/ts-tsender-ui",         // <- required
+  assetPrefix: "./",     // <- required
+  trailingSlash: true,                // <- improves static routing on GH Pages
 };
 
 export default nextConfig;
